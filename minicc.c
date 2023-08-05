f36b77aeeb3c0a9bf3ec64cd3dab05b75be7b41f
Taisuke Miyazaki <imishinist@gmail.com>
2023-08-05 23:28:44 +0900

    improve error message

--- minicc.c
+++ minicc.c
@@ -24,11 +24,27 @@ struct Token {
 // 現在着目しているトークン
 Token *token;
 
+// 入力プログラム
+char *user_input;
+
 // エラーを報告するための関数
 // printfと同じ引数を取る
 void error(char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -47,13 +63,13 @@ bool consume(char op) {
 // それ以外の場合にはエラーを報告する
 void expect(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("'%c' ではありません", op);
+        error_at(token->str, "expected '%c'", op);
     token = token->next;
 }
 
 int expect_number() {
     if (token->kind != TK_NUM)
-        error("数ではありません");
+        error_at(token->str, "expected a number");
     int val = token->val;
     token = token->next;
     return val;
@@ -71,7 +87,8 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
     return tok;
 }
 
-Token *tokenize(char *p) {
+Token *tokenize() {
+    char *p = user_input;
     Token head;
     head.next = NULL;
     Token *cur = &head;
@@ -91,7 +108,7 @@ Token *tokenize(char *p) {
             cur->val = strtol(p, &p, 10);
             continue;
         }
-        error("cannot tokenize");
+        error_at(p, "expected a number");
     }
     new_token(TK_EOF, cur, p);
     return head.next;    
@@ -103,12 +120,12 @@ int main(int argc, char **argv) {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
-    token = tokenize(argv[1]);
+    user_input = argv[1];
+    token = tokenize();
     
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-
 
     printf("  mov rax, %d\n", expect_number());
 
